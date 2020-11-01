9b33408c8392ab9133fd1d090ad46c89828b01bc
bouzuya <m@bouzuya.net>
2020-11-01 13:09:22 +0900

    improve BookmarkTitle

--- src/entity/bookmark_title.rs
+++ src/entity/bookmark_title.rs
@@ -1,12 +1,35 @@
+use std::convert::TryFrom;
+use std::str::FromStr;
+
 #[derive(Clone)]
 pub struct BookmarkTitle(String);
 
 impl BookmarkTitle {
-    pub fn from_str(s: &str) -> Option<Self> {
-        Some(Self(s.into()))
-    }
-
     pub fn to_string(&self) -> String {
-        self.0.clone()
+        self.0.to_owned()
+    }
+}
+
+impl TryFrom<&str> for BookmarkTitle {
+    type Error = &'static str;
+    fn try_from(s: &str) -> Result<Self, <Self as TryFrom<&str>>::Error> {
+        if s.len() <= 255 {
+            Ok(BookmarkTitle(s.to_owned()))
+        } else {
+            Err("Too long")
+        }
+    }
+}
+
+impl FromStr for BookmarkTitle {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
+        Self::try_from(s)
+    }
+}
+
+impl From<BookmarkTitle> for String {
+    fn from(bookmark_title: BookmarkTitle) -> Self {
+        bookmark_title.0
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@ mod repository;
 mod service;
 mod use_case;
 
-use crate::entity::bookmark_comment::BookmarkComment;
-use crate::entity::bookmark_title::BookmarkTitle;
 use crate::entity::bookmark_url::BookmarkUrl;
-use crate::entity::mail_address::MailAddress;
 use crate::entity::password::Password;
 use crate::entity::verify_user_secret::VerifyUserSecret;
 use crate::fake::fake_env::FakeEnv;
@@ -17,7 +14,7 @@ use crate::use_case::verify_user_use_case::{UseVerifyUserUseCase, VerifyUserUseC
 use anyhow::Result;
 
 fn create_user<T: UseCreateUserUseCase>(env: &T) -> Result<()> {
-    let mail_address = "m@bouzuya.net".parse::<MailAddress>().unwrap();
+    let mail_address = "m@bouzuya.net".parse().unwrap();
     let password = Password::from_str("password").unwrap();
     env.create_user_use_case()
         .create_user(mail_address, password)
@@ -30,8 +27,8 @@ fn verify_user<T: UseVerifyUserUseCase>(env: &T) -> Result<()> {
 
 fn create_bookmark<T: UseCreateBookmarkUseCase>(env: &T) -> Result<()> {
     let url = BookmarkUrl::from_str("https://bouzuya.net").unwrap();
-    let title = BookmarkTitle::from_str("bouzuya.net").unwrap();
-    let comment = "bouzuya's webpage".parse::<BookmarkComment>().unwrap();
+    let title = "bouzuya.net".parse().unwrap();
+    let comment = "bouzuya's webpage".parse().unwrap();
     env.create_bookmark_use_case()
         .create_bookmark(url, title, comment)
 }
