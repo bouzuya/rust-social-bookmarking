e73371469694ac8da9aca438ffbf9165bca4f747
bouzuya <m@bouzuya.net>
2020-11-03 08:19:38 +0900

    sort fn

--- src/main.rs
+++ src/main.rs
@@ -22,25 +22,6 @@ use crate::use_case::update_password_use_case::UseUpdatePasswordUseCase;
 use crate::use_case::verify_mail_address_use_case::UseVerifyMailAddressUseCase;
 use anyhow::Result;
 
-fn sign_up<T: UseSignUpUseCase>(env: &T) -> Result<()> {
-    let mail_address = "m@bouzuya.net".parse().unwrap();
-    let password = "password".parse().unwrap();
-    env.sign_up_use_case().sign_up(mail_address, password)
-}
-
-fn create_user<T: UseCreateUserUseCase>(env: &T) -> Result<()> {
-    let verify_user_secret = "1".repeat(255).parse().unwrap();
-    env.create_user_use_case().create_user(verify_user_secret)
-}
-
-fn sign_in<T: UseSignInUseCase>(_: &T) -> Result<()> {
-    todo!()
-}
-
-fn get_current_user<T: UseGetCurrentUserUseCase>(_: &T) -> Result<()> {
-    todo!()
-}
-
 fn create_bookmark<T: UseCreateBookmarkUseCase>(env: &T) -> Result<()> {
     let url = "https://bouzuya.net".parse().unwrap();
     let title = "bouzuya.net".parse().unwrap();
@@ -49,23 +30,32 @@ fn create_bookmark<T: UseCreateBookmarkUseCase>(env: &T) -> Result<()> {
         .create_bookmark(url, title, comment)
 }
 
-fn list_bookmarks<T: UseListBookmarksUseCase>(_: &T) -> Result<()> {
-    todo!()
-}
-
-fn update_bookmark<T: UseUpdateBookmarkUseCase>(_: &T) -> Result<()> {
-    todo!()
+fn create_user<T: UseCreateUserUseCase>(env: &T) -> Result<()> {
+    let verify_user_secret = "1".repeat(255).parse().unwrap();
+    env.create_user_use_case().create_user(verify_user_secret)
 }
 
 fn delete_bookmark<T: UseDeleteBookmarkUseCase>(_: &T) -> Result<()> {
     todo!()
 }
 
-fn update_mail_address<T: UseUpdateMailAddressUseCase>(_: &T) -> Result<()> {
+fn delete_user<T: UseDeleteUserUseCase>(_: &T) -> Result<()> {
     todo!()
 }
 
-fn verify_mail_address<T: UseVerifyMailAddressUseCase>(_: &T) -> Result<()> {
+fn get_current_user<T: UseGetCurrentUserUseCase>(_: &T) -> Result<()> {
+    todo!()
+}
+
+fn list_bookmarks<T: UseListBookmarksUseCase>(_: &T) -> Result<()> {
+    todo!()
+}
+
+fn reset_password<T: UseResetPasswordUseCase>(_: &T) -> Result<()> {
+    todo!()
+}
+
+fn sign_in<T: UseSignInUseCase>(_: &T) -> Result<()> {
     todo!()
 }
 
@@ -73,7 +63,17 @@ fn sign_out<T: UseSignOutUseCase>(_: &T) -> Result<()> {
     todo!()
 }
 
-fn reset_password<T: UseResetPasswordUseCase>(_: &T) -> Result<()> {
+fn sign_up<T: UseSignUpUseCase>(env: &T) -> Result<()> {
+    let mail_address = "m@bouzuya.net".parse().unwrap();
+    let password = "password".parse().unwrap();
+    env.sign_up_use_case().sign_up(mail_address, password)
+}
+
+fn update_bookmark<T: UseUpdateBookmarkUseCase>(_: &T) -> Result<()> {
+    todo!()
+}
+
+fn update_mail_address<T: UseUpdateMailAddressUseCase>(_: &T) -> Result<()> {
     todo!()
 }
 
@@ -85,7 +85,7 @@ fn update_password_by_secret<T: UseUpdatePasswordBySecretUseCase>(_: &T) -> Resu
     todo!()
 }
 
-fn delete_user<T: UseDeleteUserUseCase>(_: &T) -> Result<()> {
+fn verify_mail_address<T: UseVerifyMailAddressUseCase>(_: &T) -> Result<()> {
     todo!()
 }
 
