bb14c204148723a74842a146b4cce6df9b4664a8
bouzuya <m@bouzuya.net>
2020-11-17 06:18:27 +0900

    impl PgUserRepository::create_id

--- src/pg/mod.rs
+++ src/pg/mod.rs
@@ -1,8 +1,21 @@
 use crate::entity::{CredentialId, User, UserId, UserKey};
 use crate::repository::UserRepository;
 use anyhow::Result;
+use diesel::prelude::*;
+use diesel::sql_types::*;
+use std::convert::TryFrom;
 
-struct PgUserRepository;
+struct PgUserRepository {
+  connection: PgConnection,
+}
+
+impl PgUserRepository {
+  fn new(connection: PgConnection) -> Self {
+    Self { connection }
+  }
+}
+
+sql_function!(fn nextval(x: Text) -> BigInt);
 
 impl UserRepository for PgUserRepository {
   fn create(&self, _: &User) -> Result<()> {
@@ -10,7 +23,8 @@ impl UserRepository for PgUserRepository {
   }
 
   fn create_id(&self) -> Result<UserId> {
-    todo!()
+    let id = diesel::select(nextval("user_id")).get_result::<i64>(&self.connection)?;
+    UserId::try_from(id as i32).map_err(anyhow::Error::msg)
   }
 
   fn delete(&self, _: &UserId) -> Result<()> {
@@ -26,12 +40,19 @@ impl UserRepository for PgUserRepository {
   }
 }
 
-#[cfg(tests)]
+#[cfg(test)]
 mod tests {
   use super::*;
 
   #[test]
-  fn name() {
-    unimplemented!();
+  fn test_user_id() {
+    dotenv::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let connection = PgConnection::establish(&database_url)
+      .expect(&format!("Error connecting to {}", database_url));
+    let repository = PgUserRepository::new(connection);
+    let id1 = repository.create_id().expect("id");
+    let id2 = repository.create_id().expect("id");
+    assert_ne!(id1, id2);
   }
 }
