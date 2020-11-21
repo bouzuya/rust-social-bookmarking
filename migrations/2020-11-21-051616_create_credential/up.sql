3150cba50060d7a9cf01b39bf30ee6f83b2a5013
bouzuya <m@bouzuya.net>
2020-11-21 14:24:49 +0900

    diesel migration generate create_credential

---
+++ migrations/2020-11-21-051616_create_credential/up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE credential(
+  id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  mail_address TEXT NOT NULL,
+  password TEXT NOT NULL,
+  status INTEGER NOT NULL,
+  CONSTRAINT credential_pkey PRIMARY KEY (id),
+  CONSTRAINT credential_mail_address_unique UNIQUE (mail_address),
+  CONSTRAINT credential_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id)
+);
