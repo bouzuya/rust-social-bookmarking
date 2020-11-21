6809213c3470cf17f4a239fc2db89d06827d33ac
bouzuya <m@bouzuya.net>
2020-11-21 14:25:54 +0900

    diesel migration generate create_credential_password_reset

---
+++ migrations/2020-11-21-052117_create_credential_password_reset/down.sql
@@ -0,0 +1,1 @@
+DROP TABLE credential_password_reset;
---
+++ migrations/2020-11-21-052117_create_credential_password_reset/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE credential_password_reset(
+  credential_id INTEGER NOT NULL,
+  secret TEXT NOT NULL,
+  expired_at TIMESTAMP NOT NULL,
+  CONSTRAINT credential_password_reset_pkey PRIMARY KEY (credential_id),
+  CONSTRAINT credential_password_reset_secret_unique UNIQUE (secret),
+  CONSTRAINT credential_password_reset_credential_id_fkey FOREIGN KEY (credential_id) REFERENCES credential(id)
+);
