edab317e23466d775d933cf28319f78606bde6a0
bouzuya <m@bouzuya.net>
2020-11-21 14:25:07 +0900

    diesel migration generate create_credential_verified

---
+++ migrations/2020-11-21-051725_create_credential_verified/up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE credential_verified(
+  credential_id INTEGER NOT NULL,
+  verified_at TIMESTAMP NOT NULL,
+  CONSTRAINT credential_verified_pkey PRIMARY KEY (credential_id),
+  CONSTRAINT credential_verified_credential_id_fkey FOREIGN KEY (credential_id) REFERENCES credential(id)
+);
