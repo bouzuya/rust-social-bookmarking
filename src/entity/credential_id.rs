ab474e29d57b85c9975930b265ceed66ea40b3d4
bouzuya <m@bouzuya.net>
2020-11-27 07:24:32 +0900

    impl Display for CredentialId

--- src/entity/credential_id.rs
+++ src/entity/credential_id.rs
@@ -19,3 +19,22 @@ impl From<CredentialId> for i32 {
         id.0
     }
 }
+
+impl std::fmt::Display for CredentialId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_string() {
+        use std::convert::TryInto;
+        let credential_id: CredentialId = 1_i32.try_into().unwrap();
+        assert_eq!("1".to_owned(), credential_id.to_string());
+        assert_eq!("1".to_owned(), format!("{}", credential_id));
+    }
+}
