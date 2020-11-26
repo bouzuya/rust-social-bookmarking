9bd741cec90a9f8de52573334a812b6f6d6eb888
bouzuya <m@bouzuya.net>
2020-11-27 07:25:01 +0900

    add CredentialVerifiedAt

---
+++ src/entity/credential_verified_at.rs
@@ -0,0 +1,23 @@
+use chrono::prelude::*;
+use chrono::Duration;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct CredentialVerifiedAt(DateTime<Utc>);
+
+impl CredentialVerifiedAt {
+    pub fn new() -> Self {
+        Self(Utc::now())
+    }
+}
+
+impl From<NaiveDateTime> for CredentialVerifiedAt {
+    fn from(dt: NaiveDateTime) -> Self {
+        Self(DateTime::from_utc(dt, Utc))
+    }
+}
+
+impl From<CredentialVerifiedAt> for NaiveDateTime {
+    fn from(verified_at: CredentialVerifiedAt) -> Self {
+        verified_at.0.naive_utc()
+    }
+}
