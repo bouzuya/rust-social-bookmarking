bbe6e502f98b25782522ccc1db3dc37a874f198a
bouzuya <m@bouzuya.net>
2020-12-02 07:14:38 +0900

    impl sign-in for cli

--- src/main.rs
+++ src/main.rs
@@ -130,6 +130,20 @@ fn run() -> Result<()> {
                 .arg(clap::Arg::with_name("SECRET").help("secret").required(true)),
         )
         .subcommand(
+            clap::SubCommand::with_name("sign-in")
+                .about("sign-in")
+                .arg(
+                    clap::Arg::with_name("MAIL_ADDRESS")
+                        .help("mail address")
+                        .required(true),
+                )
+                .arg(
+                    clap::Arg::with_name("PASSWORD")
+                        .help("password")
+                        .required(true),
+                ),
+        )
+        .subcommand(
             clap::SubCommand::with_name("sign-up")
                 .about("sign-up")
                 .arg(
@@ -152,6 +166,19 @@ fn run() -> Result<()> {
                 .parse()
                 .map_err(anyhow::Error::msg)?;
             env.create_user_use_case().create_user(secret)?;
+        }
+        ("sign-in", Some(sub_matches)) => {
+            let mail_address = sub_matches
+                .value_of("MAIL_ADDRESS")
+                .unwrap()
+                .parse()
+                .map_err(anyhow::Error::msg)?;
+            let password = sub_matches
+                .value_of("PASSWORD")
+                .unwrap()
+                .parse()
+                .map_err(anyhow::Error::msg)?;
+            env.sign_in_use_case().sign_in(&mail_address, &password)?;
         }
         ("sign-up", Some(sub_matches)) => {
             let mail_address = sub_matches
